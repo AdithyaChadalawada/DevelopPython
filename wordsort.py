a69a96f5b367cbc37e0922320e1e03877ed23272
AdithyaChadalawada <45225328+AdithyaChadalawada@users.noreply.github.com>
2018-12-04 12:38:50 +0530

    Create wordsort.py

---
+++ wordsort.py
@@ -0,0 +1,14 @@
+#words = raw_input("Enter the words : ").split(',')
+
+'''
+for i in words:
+        words.sort()
+
+print words
+'''
+
+words = [x for x in raw_input("Enter the words : ").split(',')]
+words.sort()
+
+print words
+~                                    
